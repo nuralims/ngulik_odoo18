947ebdb690fbe2f31c67b5ab5d7b2b2827e45ff4
nuralims <alimsyah071002@gmail.com>
2026-01-23 09:39:28 +0700

    first commit

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estate",
+    "version": "1.0",
+    "category": "Real Estate",
+    "author": "Muhammad Nuralimsyah",
+    "website": "github.com/nuralims",
+    "description": "Real Estate",
+    "depends": [
+        "base"
+    ],
+    "application": True,
+}
